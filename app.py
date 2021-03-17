4569c42222675f8948e701716fb3356d3d0e95a5
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2021-03-17 11:09:27 +0000

    Initial commit: Project Start

---
+++ app.py
@@ -0,0 +1,25 @@
+"""A very simple RESTful API example
+"""
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort
+
+app = Flask(__name__)
+api = Api(app)
+
+## Resources ##
+# Use classes that inherit from Resource to define resources
+# Let's ensure the "RouteOne" resource can handle "GET" requests
+
+
+class RouteOne(Resource):
+    def get(self):
+        return {"data": "RouteOne: GET"}
+
+
+## API Routing ##
+# Now add RouteOne to the api and define the endpoint - in this case it is '/'
+api.add_resource(RouteOne, '/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
