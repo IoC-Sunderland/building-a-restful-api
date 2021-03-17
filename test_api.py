283254c607e649eb7417a448e06264111b6573a9
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2021-03-17 11:16:59 +0000

    Second Commit: Added test_api.py to test API and pip installed requests library

---
+++ test_api.py
@@ -0,0 +1,22 @@
+"""Testing the very simple RESTful API
+
+Note: Ensure Flask server is running on http://127.0.0.1:5000/
+      before running this script.
+"""
+
+import requests
+
+BASE_URL = "http://127.0.0.1:5000"
+
+# Issue a GET Request to RouteOne
+response = requests.get(BASE_URL)
+
+# Check response is as expected
+print(response.text)
+
+# Expected response
+#
+# {
+#    "data": "RouteOne: GET"
+# }
+#
