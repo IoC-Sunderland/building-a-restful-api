f02d9cde41b1f020727badd8ec9b7568363eed5e
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2021-03-18 11:38:33 +0000

    Seventh Commit: More argument parsing

--- test_api.py
+++ test_api.py
@@ -12,14 +12,14 @@ BASE_URL = "http://127.0.0.1:5000"
 # Let's try sending some invalid data to RouteOne
 # Notice we are sending an invalid data type "age" argument here (str):
 response = requests.post(
-    BASE_URL, data={"name":"sue", "age": "foo"})
+    BASE_URL, data={"name":"sue", "age": "45"})
 print(response.text)
 
 # Expected response
 
 # {
-#      "message": {
-#        "age": "Age of person is required"
-#    }
+#    "name": "sue",
+#    "age": 45,
+#    "fav_food": null
 # }
 #
